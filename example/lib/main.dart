02cc0a599500f0ba30e782ea9fcc8d68cc68148a
nixtomalon <nonixtomalon@gmail.com>
2024-02-22 20:45:48 +0800

    added documentation comments

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -13,15 +13,24 @@ class ExampleProgressTracker extends StatefulWidget {
 }
 
 class _MyAppState extends State<ExampleProgressTracker> {
+  // List of Status objects representing the steps or statuses in the progress tracker.
+  // Each Status object includes a name, icon, and an optional 'active' parameter to indicate its current status.
+  // The 'name' field represents the label or title of the status.
+  // The 'icon' field is the IconData representing the visual representation of the status.
+  // The 'active' field, when set to true, indicates that the status is currently active or completed.
+
   final List<Status> statuList = [
     Status(name: 'ORDER', icon: Icons.shopping_bag),
     Status(name: 'SHIP', icon: Icons.local_shipping),
     Status(name: 'DONE', icon: Icons.check_circle),
   ];
 
+  // The index representing the currently active step in the progress tracker.
+  // It determines which step is currently highlighted or marked as completed.
+
   int index = 0;
 
-  void onClickButton() {
+  void nextButton() {
     setState(() {
       index++;
       statuList[index].active = true;
@@ -43,11 +52,15 @@ class _MyAppState extends State<ExampleProgressTracker> {
               child: ProgressTracker(
                 currentIndex: index,
                 statusList: statuList,
+                activeColor: Colors
+                    .green, // Optional: Customize the color for active steps (default: Colors.green).
+                inActiveColor: Colors
+                    .grey, // Optional: Customize the color for inactive steps (default: Colors.grey).
               ),
             ),
             const SizedBox(height: 20),
             ElevatedButton(
-              onPressed: index != statuList.length - 1 ? onClickButton : null,
+              onPressed: index != statuList.length - 1 ? nextButton : null,
               child: const Text('NEXT'),
             )
           ],
