750cb102ccfa1fa3a46affe82278333ce4ad764f
nixtomalon <nonixtomalon@gmail.com>
2024-02-22 18:58:24 +0800

    update widget name

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:progress_tracker_example/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const ExampleProgressTracker());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
